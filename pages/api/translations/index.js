1fac63d369618b7e19ccb45cbb2cfde20d9a8759
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-01 17:01:34 +0200

    refactoring helpers

--- pages/api/translations/index.js
+++ pages/api/translations/index.js
@@ -1,24 +1,12 @@
+
 import Cors from 'cors'
+import {runMiddleware} from '../../components'
 
 // Initializing the cors middleware
 const cors = Cors({
   methods: ['GET', 'HEAD'],
 })
 
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
 
 async function handler(req, res) {
   // Run the middleware
