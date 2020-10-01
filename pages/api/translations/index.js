344ad291069bfdc4956494209146b82dbe63ca38
Adam Zygadlewicz <adam@zygadlewicz.com>
2020-10-01 16:57:48 +0200

    added cors

---
+++ pages/api/translations/index.js
@@ -0,0 +1,42 @@
+import Cors from 'cors'
+
+// Initializing the cors middleware
+const cors = Cors({
+  methods: ['GET', 'HEAD'],
+})
+
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
+
+async function handler(req, res) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  // Rest of the API logic
+  res.json({ message: 'Hello Everyone!' })
+}
+
+export default handler
+
+/**
+ * 
+
+res.status(code) - A function to set the status code. code must be a valid HTTP status code
+res.json(json) - Sends a JSON response. json must be a valid JSON object
+res.send(body) - Sends the HTTP response. body can be a string, an object or a Buffer
+res.redirect([status,] path) - Redirects to a specified path or URL. status must be a valid HTTP status code. If not specified, status defaults to "307" "Found".
+
+
+ */
